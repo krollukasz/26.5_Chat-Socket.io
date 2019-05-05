38c80f8488fafdd1f7d8aacb8a5e1d4787e6c3ab
Łukasz Król <lionel@o2.pl>
2019-05-05 16:10:40 +0200

    Add class for user service

--- UsersService.js
+++ UsersService.js
@@ -3,18 +3,22 @@ class UsersService {
     this.users = [];
   }
 
+  // zwrócenie tablicy użytkowników
   getAllUsers() {
     return this.users;
   }
 
+  // odszukanie użytkownika po wskazanym id
   getUserById(userId) {
     return this.users.find(user => user.id === userId);
   }
 
+  // dodanie kolejnego użytkownika do listy
   addUser(user) {
     this.users = [user, ...this.users];
   }
 
+  // usuwanie wskazanego przez nas użytkownika
   removeUser(userId) {
     this.users = this.users.filter(user => user.id !== userId);
   }
