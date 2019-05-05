5b4f01309bf475943b7849bb3264dc54df38357c
Łukasz Król <lionel@o2.pl>
2019-05-05 19:09:10 +0200

    Add neccessary declared methods in main App component

--- client/App.js
+++ client/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import io from "socket.io-client";
+import styles from "./App.css";
+
+import MessageForm from "./MessageForm";
+import MessageList from "./MessageList";
+import UsersList from "./UsersList";
+import UserForm from "./UserForm";
 
 const socket = io("/");
 
